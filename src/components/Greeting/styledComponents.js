fa95399b2e30b3467abad773e98d95702f78c106
Dhanalaxmi27 <dhana2720503@gmail.com>
2024-10-31 10:31:35 +0000

    multilingualGreetings

---
+++ src/components/Greeting/styledComponents.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const ButtonContainer = styled.button`
+  width: 94px;
+  height: 35px;
+  border-radius: 20px;
+  border: solid 1px #db1c48;
+  color: #db1c48;
+  background-color: white;
+  padding: 10px;
+  margin: 10px;
+  text-align: center;
+
+  cursor: pointer;
+`
+export default ButtonContainer
